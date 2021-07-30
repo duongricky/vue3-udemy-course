2feed19429cccb9f57c19c14972901b8968d3eb8
st-duongnguyen3 <duong.nguyen3@supremetech.vn>
2021-07-30 17:16:31 +0700

    done section 4

---
+++ reaction-timer/src/App.vue
@@ -0,0 +1,45 @@
+<template>
+  <h1>Ninja Reaction Timer</h1>
+  <button @click="start" :disabled="isPlaying">Play</button>
+  <Block v-if="isPlaying" :delay="delay" @end="endGame"/>
+  <Results v-if="showResults" :score="score"/>
+</template>
+
+<script>
+import Block from './components/Block.vue'
+import Results from './components/Results.vue'
+
+
+export default {
+  name: 'App',
+  components: {
+    Block,
+    Results
+  },
+  data() {
+    return {
+      isPlaying: false,
+      delay: null,
+      score: null,
+      showResults: false
+    }
+  },
+  methods: {
+    start() {
+      this.isPlaying = true
+      this.delay = 2000 + Math.random() * 5000
+    },
+    endGame(reactionTime) {
+      this.isPlaying = false
+      this.score = reactionTime
+      this.showResults = true
+    }
+  }
+}
+</script>
+
+<style scoped>
+  * {
+    font-family: 'Courier New', Courier, monospace;
+  }
+</style>
---
+++ reaction-timer/src/components/Results.vue
@@ -0,0 +1,16 @@
+<template>
+	<h3>Your score is {{ score }}</h3>
+</template>
+
+<script>
+export default {
+  props: ["score"],
+}
+</script>
+
+<style scoped>
+	h3 {
+		color: red;
+		font-weight: bold;
+	}
+</style>
